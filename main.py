0cf4cb77239b5aeaf5e770cbab261e3840e2d0e7
Ayşenur Şirin <aysenursirin0@gmail.com>
2025-09-04 15:42:45 +0300

    made imports for the agent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def main():
-    print("Hello from project1!")
+from langchain_core.messages import HumanMessage //high level framework that allows us to build AI applications
+from langchain_openai import ChatOpenAI //allows us to use OpenAI within LangChain and LangGraph
+from langchain.tools import tool
+from langgraph.prebuilt import create_react_agent //complex framework that allows us three to build AI agents
+from dotenv import load_dotenv //load environment variables from a .env file into the system's environment variables
 
-
-if __name__ == "__main__":
-    main()
